c6bec27774cef436a7df652185cde4658e66365f
she-ll-b-rite <acroaker@outlook.com>
2019-07-12 17:01:42 +1000

    Added code samples to sample app.

---
+++ samples/reCAPTCHA AspNetCore Sample/SampleCode/SampleV2Controller.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Finoaker.Web.Recaptcha;
+using Microsoft.Extensions.Options;
+
+namespace SampleCode
+{
+    public class SampleV2Controller : Controller
+    {
+        private readonly RecaptchaSettings _settings;
+        public SampleV2Controller(IOptions<RecaptchaSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        [HttpGet]
+        public IActionResult RecaptchaV2CheckboxDemo()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult RecaptchaV2CheckboxDemo(SampleModel model)
+        {
+            // check model state first to confirm all properties, including reCAPTCHA, are valid.
+            if (ModelState.IsValid)
+            {
+                // verify the reCAPCTHA response against verification service
+                var verifyResult = RecaptchaService.VerifyTokenAsync(model.RecaptchaResponse, RecaptchaType.V2Checkbox, _settings).Result;
+
+                // check success status and score exceeds you minimum.
+                if (verifyResult.Success)
+                {
+                    // Success
+                    //...
+                } else
+                {
+                    // Fail
+                    //...
+                }
+            }
+            return View();
+        }
+    }
+}

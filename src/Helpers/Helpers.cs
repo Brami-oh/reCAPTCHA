b80c53cbef84fc1ee43b6bb2a2ab0d7e0d9ccc3b
she-ll-b-rite <acroaker@outlook.com>
2019-07-12 13:26:38 +1000

    Added doco and removed partial keyword.

--- src/Helpers/Helpers.cs
+++ src/Helpers/Helpers.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Finoaker.Web.Recaptcha
 {
-    public static partial class Helpers
+    /// <summary>
+    /// A collection of static helper functions.
+    /// </summary>
+    public static class Helpers
     {
+        /// <summary>
+        /// Gets the reCAPTCHA configuration settings from the apps <see cref="IServiceCollection"/>
+        /// </summary>
+        /// <param name="viewContext">Current <see cref="ViewContext"/></param>
+        /// <returns>reCAPTCHA configuration settings in an <see cref="RecaptchaSettings"/> object.</returns>
         internal static RecaptchaSettings GetSettings(ViewContext viewContext)
         {
             // AspNetCore specific method to retrieve RecaptchaSettings object from Config service.
